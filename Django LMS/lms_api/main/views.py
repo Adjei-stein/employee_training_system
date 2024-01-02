c33e1d1fc48a7f7df359039e00550f3336e911bf
Adjei-stein <adjeisteiner@gmail.com>
2024-01-02 18:48:17 +0000

    Created a get request with serializers

--- Django LMS/lms_api/main/views.py
+++ Django LMS/lms_api/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from main.api.serializers import EmployeeSerializer
+from rest_framework.views import APIView
+from . import models
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class EmployeeList(APIView):
+    def get(self, request):
+        employees=models.Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data)
